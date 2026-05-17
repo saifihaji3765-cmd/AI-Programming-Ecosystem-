75c256a1077fc1b89d0ae1fec00f44fd8ccb04a8
saifihaji3765-cmd <saifihaji3765@gmail.com>
2026-05-17 19:31:14 +0530

    Update app.js

--- public/app.js
+++ public/app.js
@@ -3,7 +3,11 @@
 // =========================
 
 let editor;
+// =========================
+// OPEN TABS
+// =========================
 
+let openTabs = [];
 require.config({
   paths: {
     vs: "https://cdn.jsdelivr.net/npm/monaco-editor@0.45.0/min/vs"
